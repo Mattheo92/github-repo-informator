cd857ef51013c89dd843c3147e3c0c1cabb21a64
Mattheo992 <stepniak.matt@gmail.com>
2024-07-13 09:17:40 +0200

    Initial commit

---
+++ src/main/java/com/Mattheo992/github_repo_informator/GithubRepoInformatorApplication.java
@@ -0,0 +1,13 @@
+package com.Mattheo992.github_repo_informator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoInformatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoInformatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mattheo992/github_repo_informator/GithubRepoInformatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mattheo992.github_repo_informator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoInformatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
